8af3f909b0863d74baf7c8122d166cf66f89c1e0
Mwenje <mwenje24@gmail.com>
2022-06-15 09:57:51 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((res) => res.json())
+  // console.log(res)
+  // .then(books => console.log(books))
+  .then((books) => renderBooks(books))
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach(books => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML = books.name;
     main.appendChild(h2);
   });
 }
